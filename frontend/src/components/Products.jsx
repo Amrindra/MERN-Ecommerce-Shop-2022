f0f6bacb613730d1c3d95eae8871ab6c8a2cb4a1
Amrindra Ouk <amrindraouk@Amrindras-MacBook-Pro.local>
2022-05-19 13:42:13 -0400

    created useEffec for setting filter for category and sort section

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -27,11 +27,48 @@ const Products = ({ category, filters, sort }) => {
     getProducts();
   }, [category]);
 
+  //This useEffect is used for category to filter products.
+  //For EX: products have different colors and sizes and by using the filter method we can filter whatever color and size users select
+  useEffect(() => {
+    category &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, category, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        //x,y parameters are just used for comparison
+        [...prev].sort((x, y) => x.createdAt - y.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        //x,y parameters are just used for comparison
+        [...prev].sort((x, y) => x.price - y.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        //x,y parameters are just used for comparison
+        [...prev].sort((x, y) => y.price - x.price)
+      );
+    }
+  });
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <ProductItem item={item} key={item.id} />
-      ))}
+      {category
+        ? filteredProducts.map((item) => (
+            <ProductItem item={item} key={item.id} />
+          ))
+        : products
+            //.slice(0,8) will show only 8 products
+            .slice(0, 8)
+            .map((item) => <ProductItem item={item} key={item.id} />)}
     </Container>
   );
 };
