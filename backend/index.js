0e565a234cdbf569853ba8bcd3cd262e82f2bedb
Amrindra Ouk <amrindraouk@yahoo.com>
2022-05-27 15:48:36 -0400

    fixed bug at  stripe routes app.apply to app.post

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-// const stripeRoute = require("./routes/stripe");
+const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 //Have to config dotenv in order to use it
@@ -30,7 +30,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-// app.use("/api/stripe", stripeRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on ${process.env.PORT}`);
--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -1,22 +1,23 @@
-// const router = require("express").Router();
-// const stripe = require("stripe");
+const router = require("express").Router();
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
 
-// router.apply("/payment", (req, res) => {
-//   //Creating stripe payment
-//   stripe.charges.create(
-//     {
-//       source: req.body.tokenId,
-//       amount: req.body.amount,
-//       currency: "usd",
-//     },
-//     (stripeErr, stripeRes) => {
-//       if (!stripeErr) {
-//         res.status(200).json(stripeRes);
-//       } else {
-//         res.staus(500).json(stripeErr);
-//       }
-//     }
-//   );
-// });
+router.post("/payment", (req, res) => {
+  //Creating stripe payment
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (!stripeErr) {
+        res.status(200).json(stripeRes);
+      } else {
+        res.staus(500).json(stripeErr);
+      }
+    }
+  );
+});
 
-// module.exports = router;
+module.exports = router;
