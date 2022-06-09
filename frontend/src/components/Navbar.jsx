c7d3fc52408b5ab657cf3b493223a630fc29297b
Amrindra Ouk <amrindraouk@yahoo.com>
2022-06-09 15:38:55 -0400

    refactored the design and added tablet screen size

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { mobile } from "../responsive";
+import { mobile, tablet } from "../responsive";
 
 function Navbar() {
   //This state.cart is accessing the cart state inside the Redux store
@@ -17,22 +17,25 @@ function Navbar() {
     <Container>
       <Wrapper>
         <LeftSide>
-          <Language>EN</Language>
-          <SearchContainer>
-            <Input placeholder="search" />
-            <Search style={{ color: "gray", fontSize: 16 }} />
-          </SearchContainer>
+            <Logo>SHOP</Logo>
         </LeftSide>
 
         <Link to="/">
           <Center>
-            <Logo>SHOP</Logo>
+             {/* <Language>EN</Language> */}
+          <SearchContainer>
+            <Input placeholder="search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
           </Center>
         </Link>
 
         <RightSide>
-          <MenuItem>REGISTER</MenuItem>
-          <MenuItem>SIGN IN</MenuItem>
+          <SignInSignUpWrapper>
+            <MenuItem>SIGN UP</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+          </SignInSignUpWrapper>
+          
           <Link to="/cart">
             <MenuItem>
               <Badge
@@ -53,7 +56,7 @@ function Navbar() {
 const Container = styled.div`
   height: 60px;
   ${mobile({ height: "50px" })}
-`;
+  `;
 
 const Wrapper = styled.div`
   padding: 10px 20px;
@@ -67,49 +70,64 @@ const LeftSide = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
+  ${mobile({ paddingLeft: "10px" })}
+
 `;
 
-const Language = styled.span`
-  font-size: 14px;
-  cursor: pointer;
-  ${mobile({ display: "none" })}
+// const Language = styled.span`
+//   font-size: 14px;
+//   cursor: pointer;
+//   ${mobile({ display: "none" })}
+// `;
+
+const Center = styled.div`
+  flex: 2;
+  text-align: center;
+  width: 400px;
+${mobile({ width: "220px" })}
 `;
 
 const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
+  border: 1px solid black;
+  border-radius: 10px;
   display: flex;
   align-items: center;
   margin-left: 25px;
   padding: 5px;
+${mobile({ display: "flex", alignItem: "center", justifyContent: "space-between"})}
 `;
 
 const Input = styled.input`
   border: none;
-  ${mobile({ width: "50px" })}
+  outline: none;
+  width: 100%;
 `;
 
-const Center = styled.div`
-  flex: 1;
-  text-align: center;
-`;
 
 const Logo = styled.h1`
   font-weight: bold;
   ${mobile({ fontSize: "24px" })}
 `;
+
 const RightSide = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
   ${mobile({ flex: 2, justifyContent: "center" })}
+  ${tablet({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
-  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+  ${mobile({ fontSize: "12px", marginLeft: "10px"})}
 `;
+
+const SignInSignUpWrapper = styled.div`
+display: flex;
+  ${mobile({ display: "none"})}
+`
 
 export default Navbar;
